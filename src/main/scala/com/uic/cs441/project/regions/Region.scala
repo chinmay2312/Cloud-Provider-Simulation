648497632c98336694451ac6d970c7cbfe9db50e
Chinmay Gangal <chinmay2312@gmail.com>
2019-05-09 16:49:46 -0500

    Added enum Region

---
+++ src/main/scala/com/uic/cs441/project/regions/Region.scala
@@ -0,0 +1,15 @@
+package com.uic.cs441.project.regions
+
+class Region extends Enumeration {
+
+  type Region = Value
+
+  val REGION1 = Value("Region1")
+  val REGION2 = Value("Region2")
+  val REGION3 = Value("Region3")
+  val REGION4 = Value("Region4")
+  val REGION5 = Value("Region5")
+  val REGION6 = Value("Region6")
+  val REGION7 = Value("Region7")
+
+}
