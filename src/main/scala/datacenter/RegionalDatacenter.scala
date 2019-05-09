af610f5f13ece934f12fa952151fd679a8fc0ce2
Chinmay Gangal <chinmay2312@gmail.com>
2019-05-09 16:31:38 -0500

    RegionalDatacenter inherits NetworkDatacenter

--- src/main/scala/datacenter/RegionalDatacenter.scala
+++ src/main/scala/datacenter/RegionalDatacenter.scala
@@ -1,5 +1,28 @@
 package datacenter
 
-class RegionalDatacenter {
+import org.cloudbus.cloudsim.allocationpolicies.{VmAllocationPolicy, VmAllocationPolicySimple}
+import org.cloudbus.cloudsim.core.{CloudSim, Simulation}
+import org.cloudbus.cloudsim.datacenters.network.NetworkDatacenter
+import org.cloudbus.cloudsim.hosts.Host
+import java.util
 
+class RegionalDatacenter(
+                          region:String,
+                          simulation:Simulation,
+                          hostList:util.ArrayList[Host],
+                          vmAllocationPolicy:VmAllocationPolicy)
+  extends NetworkDatacenter(simulation, hostList, vmAllocationPolicy){
+
+
+}
+
+object RegionalDatacenter {
+  def main(args: Array[String]): Unit =  {
+
+    val simulation:CloudSim = new CloudSim()
+    val hostList = new util.ArrayList[Host]()
+    val vmAllocationPolicy:VmAllocationPolicy = new VmAllocationPolicySimple()
+
+    new RegionalDatacenter("Chicago", simulation, hostList, vmAllocationPolicy)
+  }
 }
