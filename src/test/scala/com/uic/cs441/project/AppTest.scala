041c93ee7e9ff5e9eb08454dfad7493d3c870d80
Chinmay Gangal <chinmay2312@gmail.com>
2019-05-11 17:51:29 -0500

    - Attempt to get cost - Added publicly accessible method to get all DCs

--- src/test/scala/com/uic/cs441/project/AppTest.scala
+++ src/test/scala/com/uic/cs441/project/AppTest.scala
@@ -1,9 +1,9 @@
 package com.uic.cs441.project
 
 import java.util
+
 import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
-
 import cloudsimplus.extension.broker.RegionalDatacenterBroker
 import cloudsimplus.extension.cloudlet.RegionalCloudlet
 import cloudsimplus.extension.vm.RegionalVm
@@ -13,8 +13,10 @@ import com.uic.cs441.project.generator.Generator.generateCloudlets
 import org.cloudbus.cloudsim.cloudlets.Cloudlet
 import org.cloudbus.cloudsim.cloudlets.Cloudlet.Status
 import org.cloudbus.cloudsim.core.CloudSim
-import org.cloudbus.cloudsim.vms.Vm
+import org.cloudbus.cloudsim.vms.{Vm, VmCost}
 import org.scalatest.FlatSpec
+
+import scala.collection.mutable
 
 class AppTest extends FlatSpec  {
 
@@ -42,6 +44,15 @@ class AppTest extends FlatSpec  {
 
   val cloudletCount:Int = getCloudletValues.countOfCloudlets
   //System.out.println(cloudletCount)
+
+  val myvmlist: List[Vm] = broker.getVmCreatedList.toList
+  var sum:Double =0
+  for(i <- 0 until myvmlist.length) {
+    sum = sum + new VmCost(myvmlist(i)).getTotalCost
+  }
+  println("Cost = "+sum)
+  //val vmCost = new VmCost(myvmlist(0))
+  //println(vmCost.getTotalCost)
 
   "MainApp" should "have all cloudlets in execution" in {
     assert(cloudletSubmittedList(cloudletCount - 1).getStatus == Status.INEXEC)
